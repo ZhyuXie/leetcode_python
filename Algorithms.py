e17e72318a22feebee8bacec15e5ce509255da8b
dakenz <zhyuxie@gmail.com>
2017-05-09 21:12:19 +0800

    Q2 Q445

--- Algorithms.py
+++ Algorithms.py
@@ -30,6 +30,63 @@ class Solution(object):
                 secnumdict[target-nums[i]] = i
         
 
+    ## Q2 ##
+    '''
+    You are given two non-empty linked lists representing two non-negative integers. 
+    The digits are stored in reverse order and each of their nodes contain a single digit. 
+    Add the two numbers and return it as a linked list.
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    '''
+    
+    # Definition for singly-linked list.
+    # class ListNode(object):
+    #     def __init__(self, x):
+    #         self.val = x
+    #         self.next = None
+
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 == None: return l2
+        if l2 == None: return l1
+
+        res = l1  #the result will be saved in l1
+        cell = 0
+        newNode = ListNode(0)
+        while 1:
+            if l1 != None and l2 != None:
+                remainder = (l1.val + l2.val + cell) % 10
+                cell = (l1.val + l2.val + cell) // 10
+                l1.val = remainder
+                if l1.next == None and l2.next != None:
+                    newNode.next = l1  # use the point of the cell node to save the point one step previous of l1
+                    l1.next = l2.next    #the rest of the l2 be a list behind the l1
+                    l1 = l1.next
+                    l2 = None
+                else:
+                    newNode.next = l1  # use the point of the cell node to save the point one step previous of l1
+                    l1 = l1.next
+                    l2 = l2.next
+            elif l1 != None:
+                remainder = (l1.val + cell) % 10
+                cell = (l1.val + cell) / 10
+                l1.val = remainder
+                newNode.next = l1  # use the point of the cell node to save the point one step previous of l1
+                l1 = l1.next
+            else:
+                break
+        if cell > 0:   #has cell in the end 
+            newNode.val = cell
+            newNode.next.next = newNode
+            newNode.next = None  #clear the point finally
+        return res
+        
+
+
+
     ## Q3 ##
     # Given a string, find the length of the longest substring without repeating characters.
     def lengthOfLongestSubstring(self, s):
@@ -56,6 +113,89 @@ class Solution(object):
             pre = i
             maxlen = max(maxlen, pre - back + 1)
         return maxlen
+
+
+    ## Q4 ##
+    '''
+    There are two sorted arrays nums1 and nums2 of size m and n respectively.
+    Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+    '''
+
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        
+
+    ## Q445 Add Two Numbers II ##
+    '''
+    You are given two non-empty linked lists representing two non-negative integers. 
+    The most significant digit comes first and each of their nodes contain a single digit. 
+    Add the two numbers and return it as a linked list.
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    '''
+
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        
+        # use two list
+        if l1 == None: return l2
+        if l2 == None: return l1
+
+        res = ListNode(None)
+        ls1 = []
+        ls2 = []
+        while l1 != None:
+            ls1.append(l1.val)
+            l1 = l1.next
+
+        while l2 != None:
+            ls2.append(l2.val)
+            l2 = l2.next
+
+        len1 = len(ls1)
+        len2 = len(ls2)
+
+        idx = 1
+        carry = 0
+        while len1 - idx >=0 or len2 - idx >= 0:
+            if len1 - idx >=0 and len2 - idx >= 0:
+                reminder = (ls1[len1-idx] + ls2[len2-idx] + carry) % 10
+                newNode = ListNode((ls1[len1-idx] + ls2[len2-idx] + carry) // 10)
+                carry = reminder
+                newNode.next = res.next
+                res.next = newNode
+            elif len1 - idx >= 0:
+                reminder = (ls1[len1-idx] + carry) % 10
+                newNode = ListNode((ls1[len1-idx] + carry) // 10)
+                carry = reminder
+                newNode.next = res.next
+                res.next = newNode
+            elif len2 - idx >= 0:
+                reminder = (ls2[len2-idx] + carry) % 10
+                newNode = ListNode((ls2[len2-idx] + carry) // 10)
+                carry = reminder
+                newNode.next = res.next
+                res.next = newNode
+            else:
+                break
+
+            idx += 1
+
+        if carry > 0:
+            newNode = ListNode(carry)
+            newNode.next = res.next
+            res.next = newNode
+
+        return res.next
+                
+
 
 
 
